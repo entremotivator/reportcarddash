8ae2cbb01042e0115d6517657b5d242bc7ea299e
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-08 17:19:10 -0500

    Update report.py

--- report.py
+++ report.py
@@ -35,95 +35,95 @@ st.sidebar.title("TruLens Dashboard Options")
 
 # Options in the sidebar
 option_show_summary = st.sidebar.checkbox("Show Summary Statistics", value=True)
-
-# Multi-select for Agents
-selected_agents = st.sidebar.multiselect("Select Agents", tru_lens_dashboard['Agent'].unique())
-
-# Multi-select for LLMS Options
-selected_llms_options = st.sidebar.multiselect("Select LLMS Options", tru_lens_dashboard['LLMS Score'].unique())
+selected_agent = st.sidebar.selectbox("Select Agent", tru_lens_dashboard['Agent'].unique())
 
 # Display the dashboard content
 st.title("TruLens Dashboard")
 
-# Filter data based on selected options
-filtered_data = tru_lens_dashboard[
-    (tru_lens_dashboard['Agent'].isin(selected_agents)) &
-    (tru_lens_dashboard['LLMS Score'].isin(selected_llms_options))
-]
-
 # Option 1: Show the DataFrame
 st.header("Option 1: Display TruLens DataFrame")
-st.dataframe(filtered_data)
+st.dataframe(tru_lens_dashboard)
 
 # Option 2: Show summary statistics
 if option_show_summary:
     st.header("Option 2: Summary Statistics")
-    st.write(filtered_data.describe())
+    st.write(tru_lens_dashboard.describe())
 
 # Option 3: Show a bar chart for LLMS Scores by Agent
 st.header("Option 3: LLMS Scores by Agent")
-bar_chart_data = filtered_data.groupby('Agent')['LLMS Score'].mean()
+bar_chart_data = tru_lens_dashboard.groupby('Agent')['LLMS Score'].mean()
 st.bar_chart(bar_chart_data)
+st.text("Details: This chart displays the average LLMS Score for each Agent.")
 
 # Option 4: Show a line chart for LLMS Scores over time (assumed time data)
 st.header("Option 4: LLMS Scores over Time")
-time_data = np.arange(len(filtered_data))
-st.line_chart(pd.DataFrame({'Time': time_data, 'LLMS Score': filtered_data['LLMS Score']}))
-
-
+time_data = np.arange(len(tru_lens_dashboard))
+st.line_chart(pd.DataFrame({'Time': time_data, 'LLMS Score': tru_lens_dashboard['LLMS Score']}))
+st.text("Details: This line chart shows how LLMS Scores change over time.")
 
 # Option 6: Show a scatter plot for LLMS Scores vs. another metric (Feature1)
 st.header("Option 6: LLMS Scores vs. Feature1")
 st.scatter_chart(pd.DataFrame({'LLMS Score': tru_lens_dashboard['LLMS Score'], 'Feature1': tru_lens_dashboard['Feature1']}))
-
+st.text("Details: This scatter plot compares LLMS Scores with Feature1 for each data point.")
 
 # Option 8: Show a number input for custom LLMS Score threshold
 st.header("Option 8: Custom LLMS Score Threshold")
 custom_threshold = st.number_input("Enter LLMS Score Threshold", min_value=0, max_value=100, value=50)
+st.text(f"Details: Displaying data where LLMS Score is greater than {custom_threshold}.")
 
 # Option 9: Show a text input for filtering by Agent
 st.header("Option 9: Filter by Agent")
 filter_agent = st.text_input("Enter Agent Name")
+st.text(f"Details: Displaying data for the specified Agent: {filter_agent}")
 
 # Option 10: Show a date input for filtering by date (assumed date data)
 st.header("Option 10: Filter by Date")
 filter_date = st.date_input("Select a Date")
+st.text(f"Details: Displaying data for the specified Date: {filter_date}")
 
 # Option 11: Show a cost chart for each agent
 st.header("Option 11: Cost by Agent")
 cost_chart_data = tru_lens_dashboard.groupby('Agent')['Cost'].sum()
 st.bar_chart(cost_chart_data)
+st.text("Details: This chart displays the total Cost for each Agent.")
 
 # Option 12: Show a bar chart for Feature3
 st.header("Option 12: Feature3 Distribution")
 feature3_chart_data = tru_lens_dashboard['Feature3'].value_counts()
 st.bar_chart(feature3_chart_data)
+st.text("Details: This chart shows the distribution of Feature3.")
 
 # Option 13: Show a bar chart for Feature4
 st.header("Option 13: Feature4 Distribution")
 feature4_chart_data = tru_lens_dashboard['Feature4'].value_counts()
 st.bar_chart(feature4_chart_data)
+st.text("Details: This chart shows the distribution of Feature4.")
 
 # Option 14: Show a line chart for Feature5
 st.header("Option 14: Feature5 over Time")
 st.line_chart(pd.DataFrame({'Time': time_data, 'Feature5': tru_lens_dashboard['Feature5']}))
+st.text("Details: This line chart shows how Feature5 changes over time.")
 
 # Option 15: Show a scatter plot for Feature6 vs. Feature7
 st.header("Option 15: Feature6 vs. Feature7")
 st.scatter_chart(pd.DataFrame({'Feature6': tru_lens_dashboard['Feature6'], 'Feature7': tru_lens_dashboard['Feature7']}))
+st.text("Details: This scatter plot compares Feature6 with Feature7 for each data point.")
 
 # Option 17: Show a number input for custom Feature9 threshold
 st.header("Option 17: Custom Feature9 Threshold")
 custom_feature9_threshold = st.number_input("Enter Feature9 Threshold", min_value=0, max_value=10, value=5)
+st.text(f"Details: Displaying data where Feature9 is greater than {custom_feature9_threshold}.")
 
 # Option 18: Show a text input for filtering by Feature11
 st.header("Option 18: Filter by Feature11")
 filter_feature11 = st.text_input("Enter Feature11 Value")
+st.text(f"Details: Displaying data for the specified Feature11 Value: {filter_feature11}")
 
-# Option 20: Show a bar chart for Feature13
-st.header("Option 20: Feature13 Distribution")
-feature13_chart_data = tru_lens_dashboard['Feature13'].value_counts()
-st.bar_chart(feature13_chart_data)
+# Option 20: Show a bar chart for Feature14
+st.header("Option 20: Feature14 Distribution")
+feature14_chart_data = tru_lens_dashboard['Feature14'].value_counts()
+st.bar_chart(feature14_chart_data)
+st.text("Details: This chart shows the distribution of Feature14.")
 
 # You can customize and add more options based on your specific needs
 
