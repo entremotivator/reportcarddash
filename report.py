0af2877d598bd2f68cb1619aa574c14bb54306d0
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-08 17:52:26 -0500

    Update report.py

--- report.py
+++ report.py
@@ -1,94 +1,124 @@
-import streamlit as st
+User
+make this UI better import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import matplotlib.pyplot as plt
 
+NUM_AGENTS = 10
+NUM_ROWS = 100
 
-# Assume `tru.run_dashboard()` returns the content of your dashboard
-def run_dashboard():
-    # Generate data with 10 agents, LLMS Score, Cost, Feature1 to Feature14
-    num_agents = 10
+def generate_data(num_agents=NUM_AGENTS, num_rows=NUM_ROWS):
     data = {
-        'Agent': np.random.choice([f'Agent {i}' for i in range(1, num_agents + 1)], 100),
-        'LLMS Score': np.random.randint(0, 100, 100),
-        'Cost': np.random.randint(1000, 5000, 100),
-        'Feature1': np.random.normal(0, 1, 100),
-        'Feature2': np.random.uniform(0, 1, 100),
-        'Feature3': np.random.choice(['A', 'B', 'C'], 100),
-        'Feature4': np.random.randint(1, 5, 100),
-        'Feature5': np.random.randn(100),
-        'Feature6': np.random.exponential(1, 100),
-        'Feature7': np.random.gamma(2, 2, 100),
-        'Feature8': np.random.logistic(0, 1, 100),
-        'Feature9': np.random.poisson(5, 100),
-        'Feature10': np.random.uniform(-1, 1, 100),
-        'Feature11': np.random.choice(['X', 'Y', 'Z'], 100),
-        'Feature12': np.random.randint(10, 20, 100),
-        'Feature13': np.random.normal(5, 2, 100),
-        'Feature14': np.random.uniform(0, 10, 100),
+        'Agent': np.random.choice([f'Agent {i}' for i in range(1, num_agents + 1)], num_rows),
+        'LLMS Score': np.random.randint(0, 100, num_rows),
+        'Cost': np.random.randint(1000, 5000, num_rows),
+        'Feature1': np.random.normal(0, 1, num_rows),
+        'Feature2': np.random.uniform(0, 1, num_rows),
+        'Feature3': np.random.choice(['A', 'B', 'C'], num_rows),
+        'Feature4': np.random.randint(1, 5, num_rows),
+        'Feature5': np.random.randn(num_rows),
+        'Feature6': np.random.exponential(1, num_rows),
+        'Feature7': np.random.gamma(2, 2, num_rows),
+        'Feature8': np.random.logistic(0, 1, num_rows),
+        'Feature9': np.random.poisson(5, num_rows),
+        'Feature10': np.random.uniform(-1, 1, num_rows),
+        'Feature11': np.random.choice(['X', 'Y', 'Z'], num_rows),
+        'Feature12': np.random.randint(10, 20, num_rows),
+        'Feature13': np.random.normal(5, 2, num_rows),
+        'Feature14': np.random.uniform(0, 10, num_rows),
     }
-    tru_lens_df = pd.DataFrame(data)
-    return tru_lens_df
+    return pd.DataFrame(data)
 
-tru_lens_dashboard = run_dashboard()
+def show_dataframe(data):
+    st.header("Option 1: Display TruLens DataFrame")
+    st.dataframe(data)
 
-# Add Streamlit sidebar
-st.sidebar.title("TruLens Dashboard Options")
-
-# Options in the sidebar
-option_show_summary = st.sidebar.checkbox("Show Summary Statistics", value=True)
-selected_agent = st.sidebar.selectbox("Select Agent", tru_lens_dashboard['Agent'].unique())
-
-# Display the dashboard content
-st.title("TruLens Dashboard")
-
-# Option 1: Show the DataFrame
-st.header("Option 1: Display TruLens DataFrame")
-st.dataframe(tru_lens_dashboard)
-
-# Option 2: Show summary statistics
-if option_show_summary:
+def show_summary_statistics(data):
     st.header("Option 2: Summary Statistics")
-    st.write(tru_lens_dashboard.describe())
+    st.write(data.describe())
 
-# Option 3: Show a bar chart for LLMS Scores by Agent
-st.header("Option 3: LLMS Scores by Agent")
-bar_chart_data = tru_lens_dashboard.groupby('Agent')['LLMS Score'].mean()
-st.bar_chart(bar_chart_data)
+def show_llms_scores_by_agent(data):
+    st.header("Option 3: LLMS Scores by Agent")
+    bar_chart_data = data.groupby('Agent')['LLMS Score'].mean()
+    st.bar_chart(bar_chart_data)
 
-# Option 4: Show an area chart for Feature2
-st.header("Option 4: Feature2 Distribution")
-area_chart_data = tru_lens_dashboard.groupby('Agent')['Feature2'].sum()
-st.area_chart(area_chart_data)
+def show_feature2_distribution(data):
+    st.header("Option 4: Feature2 Distribution")
+    area_chart_data = data.groupby('Agent')['Feature2'].sum()
+    st.area_chart(area_chart_data)
 
-# Option 5: Show a line chart for LLMS Scores over time (assumed time data)
-st.header("Option 5: LLMS Scores over Time")
-time_data = np.arange(len(tru_lens_dashboard))
-st.line_chart(pd.DataFrame({'Time': time_data, 'LLMS Score': tru_lens_dashboard['LLMS Score']}))
+def show_llms_scores_over_time(data):
+    st.header("Option 5: LLMS Scores over Time")
+    time_data = np.arange(len(data))
+    st.line_chart(pd.DataFrame({'Time': time_data, 'LLMS Score': data['LLMS Score']}))
 
-# Option 6: Show a scatter plot for LLMS Scores vs. another metric (Feature1)
-st.header("Option 6: LLMS Scores vs. Feature1")
-st.scatter_chart(pd.DataFrame({'LLMS Score': tru_lens_dashboard['LLMS Score'], 'Feature1': tru_lens_dashboard['Feature1']}))
+def show_llms_scores_vs_feature1(data):
+    st.header("Option 6: LLMS Scores vs. Feature1")
+    st.scatter_chart(pd.DataFrame({'LLMS Score': data['LLMS Score'], 'Feature1': data['Feature1']}))
 
-# Option 7: Show an Altair chart for Feature3
-st.header("Option 7: Feature3 Distribution")
-chart = alt.Chart(tru_lens_dashboard).mark_bar().encode(x='Feature3', y='count()')
-st.altair_chart(chart, use_container_width=True)
+def show_feature3_distribution(data):
+    st.header("Option 7: Feature3 Distribution")
+    chart = alt.Chart(data).mark_bar().encode(x='Feature3', y='count()')
+    st.altair_chart(chart, use_container_width=True)
 
+def show_cost_by_feature(data):
+    st.header("Option 8: Cost by Feature")
+    scatter_chart_data = pd.DataFrame({'Cost': data['Cost'], 'Feature4': data['Feature4']})
+    st.scatter_chart(scatter_chart_data)
 
-# Option 10: Show a PyDeck chart for Feature6 and Feature7
-st.header("Option 10: Feature6 vs. Feature7 (PyDeck)")
-pydeck_chart_data = pd.DataFrame({'Feature6': tru_lens_dashboard['Feature6'], 'Feature7': tru_lens_dashboard['Feature7']})
-st.pydeck_chart(pydeck_chart_data)
+def show_feature8_distribution(data):
+    st.header("Option 9: Feature8 Distribution")
+    hist_chart_data = data['Feature8']
+    st.hist_chart(hist_chart_data)
 
+def show_feature9_poisson_distribution(data):
+    st.header("Option 10: Feature9 Poisson Distribution")
+    hist_chart_data = data['Feature9']
+    st.hist_chart(hist_chart_data)
 
-# Option 12: Show a Graphviz chart (Example: Pie chart for Feature9)
-st.header("Option 12: Feature9 Distribution (Graphviz)")
-graphviz_chart_data = tru_lens_dashboard['Feature9'].value_counts()
-st.graphviz_chart(graphviz_chart_data)
+def show_feature11_distribution(data):
+    st.header("Option 11: Feature11 Distribution")
+    chart = alt.Chart(data).mark_bar().encode(x='Feature11', y='count()')
+    st.altair_chart(chart, use_container_width=True)
 
+def show_feature12_vs_feature13_scatter(data):
+    st.header("Option 12: Feature12 vs. Feature13 Scatter Plot")
+    st.scatter_chart(pd.DataFrame({'Feature12': data['Feature12'], 'Feature13': data['Feature13']}))
 
-# You can customize and add more options based on your specific needs
+def show_feature14_distribution(data):
+    st.header("Option 13: Feature14 Distribution")
+    hist_chart_data = data['Feature14']
+    st.hist_chart(hist_chart_data)
 
-# Note: Adjust the features and their representations based on your actual data and use case.
+# ... Add more chart functions similarly
+
+def main():
+    tru_lens_dashboard = generate_data()
+
+    st.sidebar.title("TruLens Dashboard Options")
+    option_show_summary = st.sidebar.checkbox("Show Summary Statistics", value=True)
+    
+    # ... Add other sidebar options
+    
+    st.title("TruLens Dashboard")
+
+    show_dataframe(tru_lens_dashboard)
+    
+    if option_show_summary:
+        show_summary_statistics(tru_lens_dashboard)
+    
+    show_llms_scores_by_agent(tru_lens_dashboard)
+    show_feature2_distribution(tru_lens_dashboard)
+    show_llms_scores_over_time(tru_lens_dashboard)
+    show_llms_scores_vs_feature1(tru_lens_dashboard)
+    show_feature3_distribution(tru_lens_dashboard)
+    show_cost_by_feature(tru_lens_dashboard)
+    show_feature8_distribution(tru_lens_dashboard)
+    show_feature9_poisson_distribution(tru_lens_dashboard)
+    show_feature11_distribution(tru_lens_dashboard)
+    show_feature12_vs_feature13_scatter(tru_lens_dashboard)
+    show_feature14_distribution(tru_lens_dashboard)
+    # ... Add more chart functions
+
+if __name__ == "__main__":
+    main()
