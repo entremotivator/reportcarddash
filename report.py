a1e3c1a9e4fecac269c06e9680fcd98a0862cf3c
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-08 16:28:38 -0500

    Update report.py

--- report.py
+++ report.py
@@ -1,75 +1,90 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from trulens_eval import Tru, Feedback, Select
+from trulens_eval.feedback import Groundedness
+from trulens_eval.feedback.provider.openai import OpenAI as fOpenAI
+from trulens import Rag  # Assuming 'Rag' is the correct class, adjust as needed
+from trulens_eval import Feedback, LiteLLM, Tru, TruChain, Huggingface
 
-# Assume `tru.run_dashboard()` returns the content of your dashboard
-def run_dashboard():
-    # You can replace this with the actual TruLens dashboard content
-    # For demonstration purposes, we'll create a simple DataFrame
-    data = {
-        'Agent': np.random.choice(['Agent 1', 'Agent 2', 'Agent 3'], 100),
-        'LLMS Score': np.random.randint(0, 100, 100),
-    }
-    tru_lens_df = pd.DataFrame(data)
-    return tru_lens_df
+def run_gpt_report_card():
+    # Assume `gpt.run_report_card()` returns the content of your GPT Report Card
+    def generate_data():
+        data = {
+            'Agent': np.random.choice(['Agent 1', 'Agent 2', 'Agent 3'], 100),
+            'GPT Score': np.random.randint(0, 100, 100),
+            'Groundedness Feedback': Groundedness().provide('Some generated text'),
+            'LiteLLM Feedback': LiteLLM().provide('Some generated text'),
+        }
+        gpt_report_card_df = pd.DataFrame(data)
+        return gpt_report_card_df
 
-tru_lens_dashboard = run_dashboard()
+    gpt_report_card = generate_data()
 
-# Add Streamlit sidebar
-st.sidebar.title("TruLens Dashboard Options")
+    # Add Streamlit sidebar
+    st.sidebar.title("GPT Report Card Options")
 
-# Options in the sidebar
-option_show_summary = st.sidebar.checkbox("Show Summary Statistics", value=True)
-selected_agent = st.sidebar.selectbox("Select Agent", tru_lens_dashboard['Agent'].unique())
+    # Options in the sidebar
+    option_show_summary = st.sidebar.checkbox("Show Summary Statistics", value=True)
+    selected_agent = st.sidebar.selectbox("Select Agent", gpt_report_card['Agent'].unique())
 
-# Display the dashboard content
-st.title("TruLens Dashboard")
+    # Display the GPT Report Card content
+    st.title("GPT Report Card")
 
-# Option 1: Show the DataFrame
-st.header("Option 1: Display TruLens DataFrame")
-st.dataframe(tru_lens_dashboard)
+    # Feature 1: Show the DataFrame
+    st.header("Feature 1: Display GPT Report Card DataFrame")
+    st.dataframe(gpt_report_card)
 
-# Option 2: Show summary statistics
-if option_show_summary:
-    st.header("Option 2: Summary Statistics")
-    st.write(tru_lens_dashboard.describe())
+    # Feature 2: Show summary statistics
+    if option_show_summary:
+        st.header("Feature 2: Summary Statistics")
+        st.write(gpt_report_card.describe())
 
-# Option 3: Show a bar chart for LLMS Scores by Agent
-st.header("Option 3: LLMS Scores by Agent")
-bar_chart_data = tru_lens_dashboard.groupby('Agent')['LLMS Score'].mean()
-st.bar_chart(bar_chart_data)
+    # Feature 3: Show a bar chart for GPT Scores by Agent
+    st.header("Feature 3: GPT Scores by Agent")
+    bar_chart_data = gpt_report_card.groupby('Agent')['GPT Score'].mean()
+    st.bar_chart(bar_chart_data)
 
-# Option 4: Show a line chart for LLMS Scores over time (assumed time data)
-st.header("Option 4: LLMS Scores over Time")
-time_data = np.arange(len(tru_lens_dashboard))
-st.line_chart(pd.DataFrame({'Time': time_data, 'LLMS Score': tru_lens_dashboard['LLMS Score']}))
+    # Feature 4: Show a line chart for GPT Scores over time (assumed time data)
+    st.header("Feature 4: GPT Scores over Time")
+    time_data = np.arange(len(gpt_report_card))
+    st.line_chart(pd.DataFrame({'Time': time_data, 'GPT Score': gpt_report_card['GPT Score']}))
 
-# Option 5: Show a pie chart for the distribution of LLMS Scores
-st.header("Option 5: Distribution of LLMS Scores")
-st.pie_chart(tru_lens_dashboard['LLMS Score'].value_counts())
+    # Feature 5: Show a pie chart for the distribution of GPT Scores
+    st.header("Feature 5: Distribution of GPT Scores")
+    st.pie_chart(gpt_report_card['GPT Score'].value_counts())
 
-# Option 6: Show a scatter plot for LLMS Scores vs. another metric
-st.header("Option 6: LLMS Scores vs. Another Metric")
-another_metric_data = np.random.randint(0, 100, 100)
-st.scatter_chart(pd.DataFrame({'LLMS Score': tru_lens_dashboard['LLMS Score'], 'Another Metric': another_metric_data}))
+    # Feature 6: Show a scatter plot for GPT Scores vs. another metric
+    st.header("Feature 6: GPT Scores vs. Another Metric")
+    another_metric_data = np.random.randint(0, 100, 100)
+    st.scatter_chart(pd.DataFrame({'GPT Score': gpt_report_card['GPT Score'], 'Another Metric': another_metric_data}))
 
-# Option 7: Show a histogram of LLMS Scores
-st.header("Option 7: Histogram of LLMS Scores")
-st.hist_chart(tru_lens_dashboard['LLMS Score'])
+    # Feature 7: Show a histogram of GPT Scores
+    st.header("Feature 7: Histogram of GPT Scores")
+    st.hist_chart(gpt_report_card['GPT Score'])
 
-# Option 8: Show a number input for custom LLMS Score threshold
-st.header("Option 8: Custom LLMS Score Threshold")
-custom_threshold = st.number_input("Enter LLMS Score Threshold", min_value=0, max_value=100, value=50)
+    # Feature 8: Show a number input for custom GPT Score threshold
+    st.header("Feature 8: Custom GPT Score Threshold")
+    custom_threshold = st.number_input("Enter GPT Score Threshold", min_value=0, max_value=100, value=50)
 
-# Option 9: Show a text input for filtering by Agent
-st.header("Option 9: Filter by Agent")
-filter_agent = st.text_input("Enter Agent Name")
+    # Feature 9: Show a text input for filtering by Agent
+    st.header("Feature 9: Filter by Agent")
+    filter_agent = st.text_input("Enter Agent Name")
 
-# Option 10: Show a date input for filtering by date (assumed date data)
-st.header("Option 10: Filter by Date")
-filter_date = st.date_input("Select a Date")
+    # Feature 10: Show a date input for filtering by date (assumed date data)
+    st.header("Feature 10: Filter by Date")
+    filter_date = st.date_input("Select a Date")
 
-# You can customize and add more options based on your specific needs
+    # Feature 11: Show Groundedness Feedback
+    st.header("Feature 11: Groundedness Feedback")
+    st.write(gpt_report_card['Groundedness Feedback'])
 
-# Note: Adjust the features and their representations based on your actual data and use case.
+    # Feature 12: Show LiteLLM Feedback
+    st.header("Feature 12: LiteLLM Feedback")
+    st.write(gpt_report_card['LiteLLM Feedback'])
 
+    # Continue adding features as needed...
+
+# Run the Streamlit app
+if __name__ == '__main__':
+    run_gpt_report_card()
