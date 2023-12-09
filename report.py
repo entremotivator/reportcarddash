82497d4882ce222ad334f26bb76e30b01f6caaf4
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-09 14:30:34 -0500

    Update report.py

--- report.py
+++ report.py
@@ -48,43 +48,7 @@ def show_analysis_page():
     st.title("Analysis Page")
     # Add your analysis options here
     # ...
-def show_dataframe(data):
-    st.header("Option 1: Display TruLens DataFrame")
-    st.dataframe(data)
 
-def show_summary_statistics(data):
-    st.header("Option 2: Summary Statistics")
-    st.write(data.describe())
-
-def show_llms_scores_by_agent(data):
-    st.header("Option 3: LLMS Scores by Agent")
-    bar_chart_data = data.groupby('Agent')['LLMS Score'].mean()
-    st.bar_chart(bar_chart_data)
-
-def show_feature2_distribution(data):
-    st.header("Option 4: Feature2 Distribution")
-    area_chart_data = data.groupby('Agent')['Feature2'].sum()
-    st.area_chart(area_chart_data)
-
-def show_llms_scores_over_time(data):
-    st.header("Option 5: LLMS Scores over Time")
-    time_data = np.arange(len(data))
-    st.line_chart(pd.DataFrame({'Time': time_data, 'LLMS Score': data['LLMS Score']}))
-
-def show_llms_scores_vs_feature1(data):
-    st.header("Option 6: LLMS Scores vs. Feature1")
-    st.scatter_chart(pd.DataFrame({'LLMS Score': data['LLMS Score'], 'Feature1': data['Feature1']}))
-
-def show_feature3_distribution(data):
-    st.header("Option 7: Feature3 Distribution")
-    chart = alt.Chart(data).mark_bar().encode(x='Feature3', y='count()')
-    st.altair_chart(chart, use_container_width=True)
-
-def show_cost_by_feature(data):
-    st.header("Option 8: Cost by Feature")
-    scatter_chart_data = pd.DataFrame({'Cost': data['Cost'], 'Feature4': data['Feature4']})
-    st.scatter_chart(scatter_chart_data)
-    
 def show_relevance_over_time(data):
     st.header("Relevance over Time")
     chart = alt.Chart(data).mark_line().encode(
@@ -97,105 +61,11 @@ def show_relevance_over_time(data):
     )
     st.altair_chart(chart)
 
-def show_groundedness_over_time(data):
-    st.header("Groundedness over Time")
-    chart = alt.Chart(data).mark_line().encode(
-        x='Time',
-        y='Groundedness',
-        tooltip=['Time', 'Groundedness']
-    ).properties(
-        width=800,
-        height=400
-    )
-    st.altair_chart(chart)
-
-def show_sentiment_over_time(data):
-    st.header("Sentiment over Time")
-    chart = alt.Chart(data).mark_line().encode(
-        x='Time',
-        y='Sentiment',
-        tooltip=['Time', 'Sentiment']
-    ).properties(
-        width=800,
-        height=400
-    )
-    st.altair_chart(chart)
-
-def show_model_agreement_over_time(data):
-    st.header("Model Agreement over Time")
-    chart = alt.Chart(data).mark_line().encode(
-        x='Time',
-        y='Model Agreement',
-        tooltip=['Time', 'Model Agreement']
-    ).properties(
-        width=800,
-        height=400
-    )
-    st.altair_chart(chart)
-
-def show_language_match_over_time(data):
-    st.header("Language Match over Time")
-    chart = alt.Chart(data).mark_line().encode(
-        x='Time',
-        y='Language Match',
-        tooltip=['Time', 'Language Match']
-    ).properties(
-        width=800,
-        height=400
-    )
-    st.altair_chart(chart)
-
-def show_toxicity_over_time(data):
-    st.header("Toxicity over Time")
-    chart = alt.Chart(data).mark_line().encode(
-        x='Time',
-        y='Toxicity',
-        tooltip=['Time', 'Toxicity']
-    ).properties(
-        width=800,
-        height=400
-    )
-    st.altair_chart(chart)
-
-def show_moderation_over_time(data):
-    st.header("Moderation over Time")
-    chart = alt.Chart(data).mark_line().encode(
-        x='Time',
-        y='Moderation',
-        tooltip=['Time', 'Moderation']
-    ).properties(
-        width=800,
-        height=400
-    )
-    st.altair_chart(chart)
-
-def show_stereotypes_over_time(data):
-    st.header("Stereotypes over Time")
-    chart = alt.Chart(data).mark_line().encode(
-        x='Time',
-        y='Stereotypes',
-        tooltip=['Time', 'Stereotypes']
-    ).properties(
-        width=800,
-        height=400
-    )
-    st.altair_chart(chart)
-
-def show_summarization_over_time(data):
-    st.header("Summarization over Time")
-    chart = alt.Chart(data).mark_line().encode(
-        x='Time',
-        y='Summarization',
-        tooltip=['Time', 'Summarization']
-    ).properties(
-        width=800,
-        height=400
-    )
-    st.altair_chart(chart)
+# Add other analysis functions here
 
 def main():
     st.sidebar.title("GPT Report Dashboard")
-    
+
     # Add a file uploader to the sidebar
     uploaded_file = st.sidebar.file_uploader("Option 1: Upload a CSV file", type=["csv"])
 
@@ -206,54 +76,27 @@ def main():
 
     option_show_summary = st.sidebar.checkbox("Option 2: Show Summary Statistics", value=True)
 
-    # Option 3: API Keys
     st.sidebar.header("Option 3: API Keys")
-    
-    # OpenAI API keys
-    st.sidebar.subheader("OpenAI Keys")
-    openai_key1 = st.sidebar.text_input("OpenAI Key 1")
-    openai_key2 = st.sidebar.text_input("OpenAI Key 2")
-    openai_key3 = st.sidebar.text_input("OpenAI Key 3")
-
-    # Google Cloud API keys
-    st.sidebar.subheader("Google Cloud Keys")
-    google_key1 = st.sidebar.text_input("Google Cloud Key 1")
-    google_key2 = st.sidebar.text_input("Google Cloud Key 2")
-    google_key3 = st.sidebar.text_input("Google Cloud Key 3")
-
-    # Hugging Face API keys
-    st.sidebar.subheader("Hugging Face Keys")
-    hugging_face_key1 = st.sidebar.text_input("Hugging Face Key 1")
-    hugging_face_key2 = st.sidebar.text_input("Hugging Face Key 2")
-    hugging_face_key3 = st.sidebar.text_input("Hugging Face Key 3")
+    # ... (unchanged code)
 
     st.title("🚀 GPT Report Card 📊")
 
-    st.markdown("### TruLens Dashboard\n\n🔍 Track Language Models (LLM) and agents with detailed metrics and self-improvement skills.\n\n"
-                "✨ **Key Features:**\n"
-                "1. Real-time performance metrics 📈\n"
-                "2. Personalized improvement suggestions 🌟\n"
-                "3. Historical analysis for continuous learning 🔄\n"
-                "4. User-friendly interface for easy navigation 🖥️")
+    navigation_menu = st.sidebar.radio("Navigation", ["Dashboard", "Analysis Page"])
 
-    # Placeholder for additional content or code for your TruLens Dashboard
-    # ...
+    if navigation_menu == "Dashboard":
+        st.markdown("### TruLens Dashboard\n\n🔍 Track Language Models (LLM) and agents with detailed metrics and self-improvement skills.\n\n"
+                    "✨ **Key Features:**\n"
+                    "1. Real-time performance metrics 📈\n"
+                    "2. Personalized improvement suggestions 🌟\n"
+                    "3. Historical analysis for continuous learning 🔄\n"
+                    "4. User-friendly interface for easy navigation 🖥️")
+        
+        sample_metric = 85.5
+        st.info(f"Current Accuracy: {sample_metric}%")
 
-    # Example: Display a sample metric
-    sample_metric = 85.5
-    st.info(f"Current Accuracy: {sample_metric}%")
-
-    show_relevance_over_time(tru_lens_dashboard)
-    show_groundedness_over_time(tru_lens_dashboard)
-    show_sentiment_over_time(tru_lens_dashboard)
-    show_model_agreement_over_time(tru_lens_dashboard)
-    show_language_match_over_time(tru_lens_dashboard)
-    show_toxicity_over_time(tru_lens_dashboard)
-    show_moderation_over_time(tru_lens_dashboard)
-    show_stereotypes_over_time(tru_lens_dashboard)
-    show_summarization_over_time(tru_lens_dashboard)
-
- elif navigation_menu == "Analysis Page":
+        show_relevance_over_time(tru_lens_dashboard)
+        # Add other dashboard functions here
+    elif navigation_menu == "Analysis Page":
         show_analysis_page()
 
 if __name__ == "__main__":
