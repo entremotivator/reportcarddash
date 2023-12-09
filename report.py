2facfd351d0c8938e04084ae1529534821638482
entremotivator <104661532+entremotivator@users.noreply.github.com>
2023-12-09 15:01:49 -0500

    Update report.py

--- report.py
+++ report.py
@@ -44,37 +44,49 @@ def load_data(file):
 
 # Define your analysis functions here
 
-def show_textgpt_page():
-    st.title("TextGPT Page")
+def show_analysis_page():
+    st.title("Analysis Page")
     
-    # Add content for TextGPT page
-    st.write("This is the TextGPT Page content.")
-
-    # Display tru lens metrics charts
-    st.subheader("tru Lens Metrics")
-    
-    # Generate random data for tru lens metrics
-    tru_lens_data = {
-        "Clarity": np.random.uniform(0, 10, 100),
-        "Novelty": np.random.uniform(0, 10, 100),
-        "Intent Understanding": np.random.uniform(0, 10, 100),
-        "Ambiguity Handling": np.random.uniform(0, 10, 100),
-        "User Engagement": np.random.uniform(0, 10, 100),
-        "Error Rate": np.random.uniform(0, 10, 100),
-        "Adaptability": np.random.uniform(0, 10, 100),
-        "Bias Detection": np.random.uniform(0, 10, 100),
-        "Ambient Context Awareness": np.random.uniform(0, 10, 100),
-        "Ethical Considerations": np.random.uniform(0, 10, 100),
-    }
-    
-    tru_lens_df = pd.DataFrame(tru_lens_data)
+    # Generate random data for analysis
+    analysis_data = generate_data()
 
     # Display charts for each metric
-    for metric in tru_lens_df.columns:
+    for metric in analysis_data.columns:
         st.subheader(metric)
-        st.line_chart(tru_lens_df[metric])
+        st.line_chart(analysis_data[metric])
 
-
+def generate_data(num_agents=10, num_rows=100):
+    data = {
+        'Agent': np.random.choice([f'Agent {i}' for i in range(1, num_agents + 1)], num_rows),
+        'LLMS Score': np.random.randint(0, 100, num_rows),
+        'Cost': np.random.randint(1000, 5000, num_rows),
+        'Relevance': np.random.randint(1, 100, num_rows),
+        'Groundedness': np.random.randint(1, 100, num_rows),
+        'Sentiment': np.random.randint(1, 100, num_rows),
+        'Model Agreement': np.random.randint(1, 100, num_rows),
+        'Language Match': np.random.randint(1, 100, num_rows),
+        'Toxicity': np.random.randint(1, 100, num_rows),
+        'Moderation': np.random.randint(1, 100, num_rows),
+        'Stereotypes': np.random.randint(1, 100, num_rows),
+        'Summarization': np.random.randint(1, 100, num_rows),
+        'Embeddings Distance': np.random.randint(1, 100, num_rows),
+        'Time': np.arange(num_rows),  # Added Time variable
+        'Feature1': np.random.normal(0, 1, num_rows),
+        'Feature2': np.random.uniform(0, 1, num_rows),
+        'Feature3': np.random.choice(['A', 'B', 'C'], num_rows),
+        'Feature4': np.random.randint(1, 5, num_rows),
+        'Feature5': np.random.randn(num_rows),
+        'Feature6': np.random.exponential(1, num_rows),
+        'Feature7': np.random.gamma(2, 2, num_rows),
+        'Feature8': np.random.logistic(0, 1, num_rows),
+        'Feature9': np.random.poisson(5, num_rows),
+        'Feature10': np.random.uniform(-1, 1, num_rows),
+        'Feature11': np.random.choice(['X', 'Y', 'Z'], num_rows),
+        'Feature12': np.random.randint(10, 20, num_rows),
+        'Feature13': np.random.normal(5, 2, num_rows),
+        'Feature14': np.random.uniform(0, 10, num_rows),
+    }
+    return pd.DataFrame(data)
 def show_textgpt_page():
     st.title("TextGPT Page")
     
